7be116f6c24859c43d22c4af0668a652b8b119a7
Ryan Small <ryan.small@networkedinsights.com>
2015-08-16 19:37:06 -0500

    Use io-operations; test statement locator.

--- src/main/java/com/tinyrye/jdbc/DaoStatementLocator.java
+++ src/main/java/com/tinyrye/jdbc/DaoStatementLocator.java
@@ -4,42 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import com.tinyrye.io.InputStreamToString;
+
 public class DaoStatementLocator
 {
     private final Class daoClass;
-
+    
     public DaoStatementLocator(Class daoClass) {
         this.daoClass = daoClass;
     }
     
     public String get(String statementName) {
-        try { return toString(daoClass.getResourceAsStream(pathTo(statementName))); }
-        catch (IOException ex) { throw new RuntimeException(ex); }
+        return new InputStreamToString(daoClass.getResourceAsStream(pathTo(statementName))).runToString();
     }
-
+    
     public String pathTo(String statementName) {
         return String.format("%s.%s.sql", daoClass.getSimpleName(), statementName);
-    }
-
-    protected String toString(InputStream inputStream) throws IOException
-    {
-        InputStreamReader charReader = new InputStreamReader(inputStream);
-        try
-        {
-            StringBuilder fullRead = new StringBuilder();
-            char[] readBuf = new char[1024];
-            int readCount = 0;
-            while ((readCount = charReader.read(readBuf)) > -1) {
-                fullRead.append(readBuf, 0, readCount);
-            }
-            return fullRead.toString();
-        }
-        catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-        finally {
-            try { charReader.close(); }
-            catch(IOException ex) { /* ignore */ }
-        }
     }
 }
